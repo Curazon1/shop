42415af183e87ae4f513b2821c9c4bdaf7522b1b
Curazon1 <victor.tyurin.coral@gmail.com>
2025-04-24 02:36:16 +0300

    add-table

---
+++ db/migrate/20250423232718_create_order_descriptions.rb
@@ -0,0 +1,9 @@
+class CreateOrderDescriptions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :order_descriptions do |t|
+      t.references :order, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+      t.integer :quantity
+    end
+  end
+end
